a137ba2e2ef9fbbf5aa290fa71ecd26b09f4d12c
joaoguilac <joguicosta@hotmail.com>
2023-09-15 01:39:30 -0300

    Change to long long

--- dynamic-programming/knapsack-1.cpp
+++ dynamic-programming/knapsack-1.cpp
@@ -1,30 +1,31 @@
 /**
- * @brief This is a implementation of the 0-1 knapsack problem
+ * @brief This is an implementation of the 0-1 knapsack problem
  * @link https://atcoder.jp/contests/dp/tasks/dp_d
  */
 
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long
 
-size_t knapsack(size_t n, size_t v[], size_t w[], size_t W) {
-    size_t M[n+1][W+1];
+int knapsack(int n, int v[], int w[], int W) {
+    int M[n+1][W+1];
     
-    for (size_t X = 0; X <= W; X++) {
+    for (int X = 0; X <= W; X++) {
         M[0][X] = 0;
     }
     
-    for (size_t i = 0; i <= n; i++) {
+    for (int i = 0; i <= n; i++) {
         M[i][0] = 0;
     }
 
-    for (size_t i = 1; i <= n; i++) {
-        for (size_t X = 0; X <= W; X++) {
+    for (int i = 1; i <= n; i++) {
+        for (int X = 0; X <= W; X++) {
             if (w[i] > X) {
                 M[i][X] = M[i-1][X];
             }
             else {
-                size_t use = v[i] + M[i-1][X - w[i]];
-                size_t not_use = M[i-1][X];
+                int use = v[i] + M[i-1][X - w[i]];
+                int not_use = M[i-1][X];
                 M[i][X] = max(use, not_use);
             }
         }
@@ -33,15 +34,15 @@ size_t knapsack(size_t n, size_t v[], size_t w[], size_t W) {
     return M[n][W];
 }
 
-int main(int argc, char const *argv[]) {
-    size_t n, W;
+int32_t main(int32_t argc, char const *argv[]) {
+    int n, W;
     cin >> n >> W;
     
-    size_t v[n+1], w[n+1];
+    int v[n+1], w[n+1];
     v[0] = 0;
     w[0] = 0;
-    for (size_t i = 1; i <= n; i++) {
-        size_t value, weight;
+    for (int i = 1; i <= n; i++) {
+        int value, weight;
         cin >> weight >> value;
         v[i] = value;
         w[i] = weight;
